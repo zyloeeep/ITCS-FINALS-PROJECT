4890f6d076dc1ef4dcb6c2643efe7ca002c4d3d7
zyloeeep <secocielo@gmail.com>
2024-12-16 20:04:58 +0800

    code_challenge5_Seco.py

--- code_challenge5_Seco.py
+++ code_challenge5_Seco.py
@@ -3,4 +3,5 @@ print("FARENHEIT TO CELSIUS CONVERTER PROGRAM")
 farenheit = eval(input("\nEnter temperature in Farenheit: "))
 
 celsius = ((farenheit - 32) * 5) / 9
+
 print(farenheit, "degrees Farenheit converted to celsius is ", celsius, "degrees")
