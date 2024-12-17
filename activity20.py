f8f1e5b422a9d55af35fd3f003819b9c9e3e8362
zyloeeep <secocielo@gmail.com>
2024-12-17 17:28:13 +0800

    Add files via upload

--- activity20.py
+++ activity20.py
@@ -8,22 +8,22 @@ nt = 0
 while isContinue == True:
     ask = input("Do you like to create more triangle (yes/no): ")
     if ask.lower() == "no":
-        print("Program terminated")
+        print("Program is Terminated")
         break
         isContinue = False
-
+        
     elif ask.lower() == "yes":
         os.system('cls')
-        nt  +=1
-        for x in range (1,5):
-            for r in range (1, nt+1):
-                for y in range (1, x+1):
+        nt += 1
+        for j in range(1, 5):
+            for h in range(1, nt + 1):
+                for o in range(1, j + 1):
                     print("*", end = " ")
-                for z in range (5, x, -1):
+                for n in range(5, j, -1):
                     print(" ", end = " ")
-                print(end = " ")
+                print(end=" ")
             print()
         continue
     else:
-        print("Invalid answer, please only answer in 'yes' or 'no'.")
+        print("Invalid answer, please only answer in 'yes' or 'no'")
         continue
