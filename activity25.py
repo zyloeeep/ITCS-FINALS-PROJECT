3c723d60f11aae05936acda4745211a7a1b5b76e
zyloeeep <secocielo@gmail.com>
2024-12-18 18:49:46 +0800

    Add files via upload

---
+++ activity25.py
@@ -0,0 +1,15 @@
+
+
+fruits = ["apple", "orange", "mango", "banana", "watermelon"]
+print(fruits)
+
+print(f"My favorite fruit growing up is {fruits[2]}")
+
+fruits.append("longgan")
+print(fruits)
+fruits.insert(3, "avocado")
+print(fruits)
+
+
+fruits.remove('banana')
+print(fruits)
