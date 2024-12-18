ed44ce85d75db0e841435074dd85a238b3d08233
zyloeeep <secocielo@gmail.com>
2024-12-18 18:46:36 +0800

    Add files via upload

---
+++ activity23.py
@@ -0,0 +1,7 @@
+
+
+def sum(num1, num2):
+    """This function adds the first number and second number returning sum value"""
+    return num1 + num2
+
+print(f"The sum of the numbers are: {sum(23, 24)}")
